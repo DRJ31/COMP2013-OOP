ef1009f32e230208cf293a609ee31597bfe97d58
Renjie Deng <dengrenjie31@gmail.com>
2018-05-29 13:36:48 +0800

    Update ControllerHistory

--- Project/Question12/src/ControllerHistory.java
+++ Project/Question12/src/ControllerHistory.java
@@ -2,8 +2,4 @@ public class ControllerHistory extends Controller {
     public ControllerHistory(Bank m) {
         super(m);
     }
-
-    public String create(String name, String amount, int type) {
-        return "";
-    }
 }
